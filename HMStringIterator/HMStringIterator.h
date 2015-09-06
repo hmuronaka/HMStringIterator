750b2942ece31549abd74aeeb3ffe0d1694a99d9
hmuronaka <capriccio27@gmail.com>
2015-09-06 20:25:14 +0900

    Initial Commit

---
+++ HMStringIterator/HMStringIterator.h
@@ -0,0 +1,19 @@
+//
+//  HMStringIterator.h
+//  HMStringIterator
+//
+//  Created by Muronaka Hiroaki on 2015/09/06.
+//  Copyright (c) 2015年 Muronaka Hiroaki. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HMStringIterator.
+FOUNDATION_EXPORT double HMStringIteratorVersionNumber;
+
+//! Project version string for HMStringIterator.
+FOUNDATION_EXPORT const unsigned char HMStringIteratorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HMStringIterator/PublicHeader.h>
+
+
